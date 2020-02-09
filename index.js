438f50693c8bbe26ab7b6bf01a1024b46b873f8b
next-davidiglesias <david.iglesias.zavala.next@bbva.com>
2020-02-09 21:15:22 +0100

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ import {html, render} from 'lit-html';
 const myTemplate = (name) => 
    html`<div>Hello ${name}</div>`;
 
-render(myTemplate('lit-html'), document.body);
+const myHeader = () => html `<h1> Hi I'm a header </h1>`;
+
+const myFooter = (name) => html `<p> Hi I'm a footer for ${name} </p>`;
+
+render(myHeader(), document.body.querySelector('header')
+);
+
+render(myTemplate('lit-html'), document.body.querySelector('section')
+);
+
+render(myFooter('lit-html'), document.body.querySelector('footer')
+);
